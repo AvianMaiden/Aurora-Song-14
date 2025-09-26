7eb76f1236e0c38f15e68fdd7517923a0f89411f
BudaLope <65098677+BudaLope@users.noreply.github.com>
2025-09-26 00:37:15 -0400

    Fixed names in some chems, fixed effect list being empty and no longer using settings.Toggles (which was dumb)

--- Content.Shared/_AS/Consent/EntityEffects/ConsentCondition.cs
+++ Content.Shared/_AS/Consent/EntityEffects/ConsentCondition.cs
@@ -1,14 +1,15 @@
 using Content.Shared._Floof.Consent;
 using Content.Shared.EntityEffects;
 using Content.Shared.Mind;
-using Content.Shared.Mind.Components;
 using Robust.Shared.Prototypes;
-using Robust.Shared.Toolshed.Commands.Values;
 
 namespace Content.Shared._AS.Consent.EntityEffects;
 
 public sealed partial class Consent : EntityEffectCondition
 {
+    [DataField]
+    public List<ProtoId<ConsentTogglePrototype>> EffectTypes = default!;
+
     public override bool Condition(EntityEffectBaseArgs args)
     {
         if (!args.EntityManager.System<SharedMindSystem>().TryGetMind(args.TargetEntity, out _, out var mind))
@@ -20,12 +21,12 @@ public sealed partial class Consent : EntityEffectCondition
         if (!args.EntityManager.System<SharedConsentSystem>().TryGetConsent(session.UserId, out var settings))
             return false;
 
-        foreach (var toggle in settings.Toggles)
+        foreach (var effect in EffectTypes)
         {
-            if (settings is not null && args.EntityManager.System<SharedConsentSystem>().HasConsent(settings, toggle.Key.Id))
-                return false;
+            if (settings is not null && args.EntityManager.System<SharedConsentSystem>().HasConsent(settings, effect))
+                return true;
         }
-        return true;
+        return false;
     }
 
     public override string GuidebookExplanation(IPrototypeManager prototype)
